c044fc12cc0a20ea00f9fd07c62274519e7ad18f
Dave <davearonmwan@gmail.com>
2024-05-06 23:35:46 +0100

    add oauth redirection

--- src/components/OAuthRedirectRoute.tsx
+++ src/components/OAuthRedirectRoute.tsx
@@ -1,7 +1,19 @@
+import { Navigate } from "react-router-dom";
+
+import { useUserQuery } from "@hooks/useUserQuery";
+
 export function OAuthRedirectRoute() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      Redirecting...
-    </div>
-  );
+  const { data: currentUser, isLoading: isCurrentUserLoading } =
+    useUserQuery(0);
+
+  if (isCurrentUserLoading)
+    return (
+      <div className="h-screen flex items-center justify-center">
+        Redirecting...
+      </div>
+    );
+
+  if (!currentUser) return <Navigate to="/login" />;
+
+  return <Navigate to={`/dashboard/${currentUser.$id}/`} />;
 }
